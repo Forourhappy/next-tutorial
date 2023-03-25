48a6b448731e8962b94b0d94fa7b43edec1ea8df
Forourhappy <foroh1504@gmail.com>
2023-03-26 00:52:04 +0900

    fix: firebase로 데이터 가져오기 / useSWR 사용

--- pages/data-practice/lastsales.tsx
+++ pages/data-practice/lastsales.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import useSWR from 'swr';
 
 interface SalesType {
   ['sale']: {
@@ -15,25 +16,53 @@ interface SalesListType {
 
 const LastSalesPage = () => {
   const [sales, setSales] = useState<SalesListType[]>();
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    setIsLoading(true);
-    (async () => {
-      fetch('https://fullstack-d4c28-default-rtdb.firebaseio.com/sales.json')
-        .then(res => res.json())
-        .then((data: SalesType) => {
-          const salesList: SalesListType[] = Object.entries(data).map(([key, value]) => ({
-            id: key,
-            username: value.username,
-            volume: value.volume
-          }));
-          setSales(salesList);
-          setIsLoading(false);
-        });
-    })();
-  }, []);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  if (isLoading) {
+  const {data, error}: { data: SalesType | undefined, error: Error | undefined } = useSWR(
+    'https://next-tutorial-fab2a-default-rtdb.firebaseio.com/sales.json',
+    url => fetch(url).then(res => res.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      const salesList: SalesListType[] = Object.entries(data).map(([key, value]) => ({
+        id: key,
+        username: value.username,
+        volume: value.volume
+      }));
+      setSales(salesList);
+    }
+  }, [data]);
+
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   (async () => {
+  //     fetch('https://next-tutorial-fab2a-default-rtdb.firebaseio.com//sales.json')
+  //       .then(res => res.json())
+  //       .then((data: SalesType) => {
+  //         const salesList: SalesListType[] = Object.entries(data).map(([key, value]) => ({
+  //           id: key,
+  //           username: value.username,
+  //           volume: value.volume
+  //         }));
+  //         setSales(salesList);
+  //         setIsLoading(false);
+  //       });
+  //   })();
+  // }, []);
+
+  if (!data) {
+    console.log('여기');
+    return <p>로딩 중...</p>;
+  }
+
+  if (error) {
+    return <p>로딩에 실패했습니다.</p>;
+  }
+
+  if (!data || !sales) {
+    console.log('여기인가');
     return <p>로딩 중...</p>;
   }
 
