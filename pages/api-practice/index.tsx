934af12f26294d09627237fe002afdb55bd3ed18
Forourhappy <foroh1504@gmail.com>
2023-04-05 23:59:28 +0900

    feat: loadFeedbackData

--- pages/api-practice/index.tsx
+++ pages/api-practice/index.tsx
@@ -1,6 +1,7 @@
-import React, {FormEvent, useRef} from 'react';
+import React, {FormEvent, useRef, useState} from 'react';
 
 const MainPage = () => {
+    const [feedbackItems, setFeedbackItems] = useState();
 
     const emailInputRef = useRef<HTMLInputElement>(null);
     const feedbackInputRef = useRef<HTMLTextAreaElement>(null);
@@ -22,6 +23,15 @@ const MainPage = () => {
         }).then(res => res.json())
             .then(data => console.log(data));
     }
+
+    const loadFeedbackHandler = () => {
+        fetch('/api/feedback')
+            .then(res => res.json())
+            .then((data) => {
+                setFeedbackItems(data.feedback);
+            })
+    }
+
     return (
         <div>
             <h1>메인 페이지</h1>
@@ -36,6 +46,11 @@ const MainPage = () => {
                 </div>
                 <button onClick={submitFormHandler}>제출</button>
             </form>
+            <hr/>
+            <button>불러오기</button>
+            <ul>
+                {feedbackItems.map(item => <li key={item.id}>{item.text}</li>)}
+            </ul>
         </div>
     );
 };
--- pages/api/api-practice/feedback.ts
+++ pages/api/api-practice/feedback.ts
@@ -2,27 +2,43 @@ import {NextApiRequest, NextApiResponse} from "next";
 import path from "path";
 import fs from "fs";
 
+export type FeedbackData = {
+    id: string,
+    email: string,
+    text: string
+}
 
-const handler = <PagesAPIRouteHandler>(req: NextApiRequest, res: NextApiResponse) => {
+const buildFeedbackPath = () => {
+    return path.join(process.cwd(), 'data', 'feedback.json');
+}
+
+const extractFeedback = (filePath: string) => {
+    const fileData = fs.readFileSync(filePath, 'utf8');
+    const data = JSON.parse(fileData);
+    return data;
+}
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'post') {
         const email = req.body.email;
         const feedbackText = req.body.text;
 
-        const newFeedback = {
+        const newFeedback: FeedbackData = {
             id: new Date().toDateString(),
             email: email,
             text: feedbackText
         }
 
-        const filePath = path.join(process.cwd(), 'data', 'feedback.json');
-        const fileData = fs.readFileSync(filePath, 'utf8');
-        const data = JSON.parse(fileData);
+        const filePath = buildFeedbackPath();
+        const data = extractFeedback(filePath);
         data.push(newFeedback);
         fs.writeFileSync(filePath, JSON.stringify(data));
         res.status(201).json({message: 'Success!', feedback: newFeedback})
     } else {
+        const filePath = buildFeedbackPath();
+        const data = extractFeedback(filePath);
         // 성공(200)하면 JSON형식의 파일을 반환.
-        res.status(200).json({message: 'This works!'});
+        res.status(200).json({feedback: filePath});
     }
 
 }
