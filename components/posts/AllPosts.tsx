b91effd313bd9543004bb46b7d4cae2b29859ced
Forourhappy <foroh1504@gmail.com>
2023-04-23 22:36:52 +0900

    feat: build 학습 시작

--- components/posts/AllPosts.tsx
+++ components/posts/AllPosts.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import classes from '@/styles/posts/all-posts.module.css'
-import PostList from "@/components/posts/PostList";
 
-const AllPosts = ({posts}) => {
+const AllPosts = () => {
   return (
     <section className={classes.posts}>
       <h1>All Posts</h1>
-      <PostList postList={posts}/>
     </section>
   );
 };
